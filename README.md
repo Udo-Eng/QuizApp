d43d493c82a035bf7cd82a883fdbd7f6f3759acd
Udo-Eng <udochukwuabazie.soft@gmail.com>
2022-02-16 01:27:54 +0100

    Finished the implementation of quiz App

---
+++ README.md
@@ -0,0 +1,3 @@
+                    # SIMPLE HERO QUIZ APP 
+
+This application was built with the help of site point javascript ninja to novice book by Darren jones , It has really topped my knowledge of JS and is a great book for a beginner. Order the book from amazon  [here](https://www.amazon.com/JavaScript-Novice-Ninja-Darren-Jones-dp-099538262X/dp/099538262X/ref=dp_ob_title_bk)
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const webpack = require('webpack');
+const BabiliPlugin = require("babili-webpack-plugin");
+
+module.exports = {
+    entry: './src/index.js',
+    mode: 'production',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: "defaults" }]
+                        ]
+                    }
+                }
+            }
+        ]
+    },
+}
